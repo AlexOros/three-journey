37eeb170454fd0b18858767fdeab110fb2a3addd
AlexOros <alexanderbarber1986@gmail.com>
2021-12-17 09:17:03 +0200

    Finish 02

--- 02-basic-transform/src/app.js
+++ 02-basic-transform/src/app.js
@@ -1,10 +1,8 @@
 import * as THREE from "three";
 
 const main = () => {
+  const canvas = document.getElementById("webgl");
   const scene = new THREE.Scene();
-  const geometry = new THREE.BoxGeometry(1, 1, 1);
-  const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-  const mesh = new THREE.Mesh(geometry, material);
 
   const sizes = {
     width: 800,
@@ -13,12 +11,38 @@ const main = () => {
 
   // Camera
   const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-
   camera.position.z = 3;
+  scene.add(camera);
 
-  scene.add(mesh, camera);
+  // Axis Helper
+  const axisHelper = new THREE.AxisHelper();
+  scene.add(axisHelper);
 
-  const canvas = document.getElementById("webgl");
+  //Objects
+  const group = new THREE.Group();
+  scene.add(group);
+
+  const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 0.2, 1),
+    new THREE.MeshBasicMaterial({ color: 0x225566 })
+  );
+
+  const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 0.2, 1),
+    new THREE.MeshBasicMaterial({ color: 0x22aa66 })
+  );
+
+  const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 0.2, 1),
+    new THREE.MeshBasicMaterial({ color: 0xff556f })
+  );
+
+  cube1.position.x = 1.5;
+  cube3.position.x = -1.5;
+
+  group.position.y = 1.2;
+
+  group.add(cube1, cube2, cube3);
 
   const renderer = new THREE.WebGLRenderer({
     canvas,
@@ -29,3 +53,14 @@ const main = () => {
 };
 
 main();
+
+/**
+ * Animate
+ */
+const tick = () => {
+  console.log("tick");
+
+  window.requestAnimationFrame(tick);
+};
+
+tick();
